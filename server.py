2bab8fb8c8d86657526e9b4a551cbf324c96d688
Mustafa Hoda <mustafahoda@Mustafas-MacBook-Pro.local>
2019-09-23 17:16:14 -0700

    API Working

--- server.py
+++ server.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template
 import connexion
 
 # Create the application instancre
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
 
 # Create a URL route in our application for the '/'
 @app.route('/')
@@ -10,4 +13,4 @@ def home():
     return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
